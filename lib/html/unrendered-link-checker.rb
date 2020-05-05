7248c4bf67eac8d2e2a6812af8d6b3f974995fbe
Daniel Granerud <daniel.granerud@payex.com>
2020-05-05 14:39:28 +0200

    Add spec and class

--- lib/html/unrendered-link-checker.rb
+++ lib/html/unrendered-link-checker.rb
@@ -1,6 +1,7 @@
 class Unrendered::Link::Checker < ::HTMLProofer::Check
 
     def run
+        return unless @options[:check_unrendered_link]
 
     end
 end
--- spec/unrendered-link-checker_spec.rb
+++ spec/unrendered-link-checker_spec.rb
@@ -1,1 +1,10 @@
 require "spec_helper"
+
+RSpec.describe Unrendered::Link::Checker do
+    if 'gives error when finding unrendered link' do
+        file = "#{FIXTURES_DIR}/unrendered_link.html"
+        proofer = run_proofer(file, :file, {:check_unrendered_link => true})
+        expect(proofer.failed_tests.last).to eq nil
+    end
+
+end
