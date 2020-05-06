ff2f9d5c78f95ce42a17ce2aa42bef2a11ed396f
Daniel Granerud <daniel.granerud@payex.com>
2020-05-06 07:23:51 +0200

    Use xpath to find unrendered links

--- lib/unrendered-link-checker.rb
+++ lib/unrendered-link-checker.rb
@@ -1,9 +1,10 @@
 class Chercker < ::HTMLProofer::Check
     def run
         return unless @options[:check_unrendered_link]
-        #@logger.log @html, "hello"
-        if @html.include? "]\["
-            add_issue("Content contains unrendered link: ][")
-        end
+        @html.xpath("//*[text()[contains(.,'][')]]").each do |node|
+            line = node.line
+            content = node.to_s
+            add_issue("Contains unrendered link ][!", line: line, content: content)
+          end
     end
 end
