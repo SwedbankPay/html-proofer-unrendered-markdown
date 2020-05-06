08be4c3cfc30b8b71573a2c076a29b2f49309f25
Daniel Granerud <daniel.granerud@payex.com>
2020-05-06 08:26:08 +0200

    Set version

--- lib/html/proofer/unrendered/markdown/version.rb
+++ lib/html/proofer/unrendered/markdown/version.rb
@@ -2,7 +2,8 @@ module Html
   module Proofer
     module Unrendered
       module Markdown
-        VERSION = "0.1.0"
+        @version = ENV['GEM_VERSION'] || '0.0.1-INVALID'
+        VERSION = @version
       end
     end
   end
